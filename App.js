457cb559683d7661b96deba6fb644562303b3010
Kacper Gora <kacpergoora@gmail.com>
2022-08-16 13:42:47 +0200

    Displaying task list

--- App.js
+++ App.js
@@ -2,17 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AddTask from "./components/AddTask";
-
+import TaskList from "./components/TaskList";
 export default function App() {
   const [tasks, setTasks] = useState([]);
   const addNewTask = (userInput) => {
-    setTasks(prevState => [...prevState, userInput]);
+    setTasks((prevState) => [...prevState, userInput]);
   };
- 
+
   return (
     <View style={styles.container}>
       <AddTask onAddTask={addNewTask} />
-      
+      {tasks.map((el) => {
+        return <TaskList task={el} />;
+      })}
+
       <StatusBar style="auto" />
     </View>
   );
--- components/TaskList.js
+++ components/TaskList.js
@@ -1,3 +1,10 @@
-const TaskList = () => {
-  return <View></View>;
+import { Text, View } from "react-native";
+
+const TaskList = (props) => {
+  return (
+    <View>
+      <Text>{props.task}</Text>
+    </View>
+  );
 };
+export default TaskList;
